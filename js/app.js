316ec073113bada3129c7a317cbcfcf1e258bb96
Hak269 <h2000.alawi@gmail.com>
2025-12-08 18:27:48 +0300

    getting the changabke  elements from the html

---
+++ js/app.js
@@ -0,0 +1,27 @@
+/*-------------------------------- Constants --------------------------------*/
+const attemptsElement = document.querySelectorAll('.attempt')
+const timerElement = document.querySelector('#timer')
+const currentLevelElement = document.querySelector('#current-level')
+const sqrElements = document.querySelectorAll('.sqr')
+
+
+console.log(attemptsElement)
+console.log(timerElement)
+console.log(currentLevelElement)
+console.log(sqrElements)
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
